2cd90aff807b3004b87b093d2d6a62b822f0459f
mbuziewicz <mbuziewicz@efcts.us>
2019-10-30 23:55:14 -0400

    1.4.2 Move Left and Right

--- unity_create-with-code/Prototype 1/Assets/Scripts/PlayerController.cs
+++ unity_create-with-code/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5.0f;
+    public float turnSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,5 +20,6 @@ public class PlayerController : MonoBehaviour
         // Instead of framerate, deltaTime uses the Time class and deltaTime to see how much time between frames.
         // One meter per second.  *20 = 20 meters per second.
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
     }
 }
