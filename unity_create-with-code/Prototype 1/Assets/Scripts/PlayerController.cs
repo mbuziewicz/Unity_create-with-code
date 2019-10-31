fb7f11342451a4fa9f57e2ab88f754c20c29f357
mbuziewicz <mbuziewicz@efcts.us>
2019-10-31 00:15:56 -0400

    1.4.3-4 turn speed and forward speed

--- unity_create-with-code/Prototype 1/Assets/Scripts/PlayerController.cs
+++ unity_create-with-code/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5.0f;
-    public float turnSpeed;
+    //public float turnSpeed;
+    public float turnSpeed = 5.0f;
+    public float horizontalInput;
+    public float forwardInput;
 
     // Start is called before the first frame update
     void Start()
@@ -16,10 +19,17 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Use the values from Edit/Project Settings/Input 
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
         // Move the vehicle forward
         // Instead of framerate, deltaTime uses the Time class and deltaTime to see how much time between frames.
         // One meter per second.  *20 = 20 meters per second.
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
-        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        //TUTORIAL ERROR: Without a value for turnSpeed it is 0! // 0 * whatever = 0
+        //transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+
     }
 }
