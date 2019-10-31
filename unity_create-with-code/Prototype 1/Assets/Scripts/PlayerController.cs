34438a6e97595daced7c266a47d280955271faaf
mbuziewicz <mbuziewicz@efcts.us>
2019-10-30 23:31:45 -0400

    Unity Learning with Code first commit

---
+++ unity_create-with-code/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move the vehicle forward
+        // Instead of framerate, deltaTime uses the Time class and deltaTime to see how much time between frames.
+        // One meter per second.  *20 = 20 meters per second.
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+}
